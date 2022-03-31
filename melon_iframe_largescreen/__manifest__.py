39366c93f492619631d3fbbd04a0741b26361230
melon <13655699934@163.com>
2022-03-31 18:41:34 +0800

    odoo-demo3

---
+++ melon_iframe_largescreen/__manifest__.py
@@ -0,0 +1,30 @@
+{
+    "name": "IFrame Large Screen",
+    "summary": """IFrame Large Screen""",
+    "description": """
+        H5 Large visual screen.
+    """,
+    "author": "Melon",
+    "category": "Tools/Large visual screen",
+    "website": "http://www.hxmelon.com",
+    "version": "15.0.0.1.0",
+    "license": "LGPL-3",
+    "depends": ["base"],
+    "images": ["static/description/img.png"],
+    "external_dependencies": {
+        "python": [],
+    },
+    "data": [
+        'views/actions.xml',
+        'views/menu.xml'
+    ],
+    'assets': {
+            'web.assets_backend': [
+                '/melon_iframe_largescreen/static/src/js/melon_iframe_report.js'
+            ],
+            'web.assets_qweb': [
+                '/melon_iframe_largescreen/static/src/xml/melon_iframe_report_views.xml'
+            ],
+        }
+}
+
---
+++ melon_iframe_largescreen/controllers/main.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+
+from collections import OrderedDict
+from odoo import http, _
+from odoo.http import request, route
+import os
+import jinja2
+from odoo.http import request
+from jinja2 import Environment, FileSystemLoader
+
+BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+templateLoader = FileSystemLoader(searchpath=BASE_DIR + "/static/templates")
+env = Environment(loader=templateLoader)
+
+
+class LargeScreenController(http.Controller):
+
+
+    @http.route('/iframe/bigdata', type='http', auth="public", csrf=False)
+    def big_data_html(self, **post):
+        template = env.get_template('/bigdata/pages/index.html')
+        values = {}
+        values['users'] = request.env.user
+        html = template.render(object=values)
+        return html
+
+
+    @http.route('/iframe/smart/logistics', type='http', auth="public", csrf=False)
+    def echarts_demo_html(self, **post):
+        template = env.get_template('/smart_logistics/index.html')
+        values = {}
+        values['users'] = request.env.user
+        html = template.render(object=values)
+        return html

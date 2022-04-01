5e9fe38a7a2530aa41f2a02b07836b1554b1f6af
melon <13655699934@163.com>
2022-04-01 20:30:47 +0800

    odoo14-model4

---
+++ melon_sale_print/__manifest__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+{
+    "name": "Sale Order Print",
+    "summary": """Sale Order Print""",
+    "description": """
+        Sale Order Print.
+    """,
+    "author": "Melon",
+    "category": "Tools/Sale Print",
+    "website": "http://www.hxmelon.com",
+    "version": "14.0.0.1.0",
+    "license": "LGPL-3",
+    "depends": ["base",'sale_management'],
+    "images": ["static/description/image.png"],
+    "data": [
+        'data/report_paperformat.xml',
+        'views/print_sale_order_report.xml',
+        'views/report_menu.xml'
+    ],
+}
+

f103b33e15189255ec371d04971e8eff3467925e
melon <13655699934@163.com>
2022-04-14 16:06:33 +0800

    odoo-demo4

---
+++ melon_char_search/__manifest__.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Melon Char Search",
+
+    'summary': """
+      char search
+       """,
+    'description': """
+       char search
+    """,
+    'author': "melon",
+    "website": "http://www.hxmelon.com",
+    "license": "LGPL-3",
+    'category': 'tools/search',
+    'version': '15.0.1',
+    "images": ["static/description/img.png"],
+    'depends': ['web'],
+    'currency': "USD",
+    'price': 8.88,
+    'data': [
+    ],
+    'demo': [
+    ],
+    'assets': {
+        'web.assets_backend': [
+            'melon_char_search/static/src/js/char_search.js'
+        ],
+        'web.assets_qweb': [
+            "melon_char_search/static/src/xml/base.xml"
+        ]
+    },
+    'installable': True,
+    'auto_install': False,
+    'application': False,
+}

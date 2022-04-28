0b878ccfce1c90818886bce739b9c228d2adab06
melon <13655699934@163.com>
2022-04-28 22:57:32 +0800

    odoo-demo6

--- melon_import_wizard/__manifest__.py
+++ melon_import_wizard/__manifest__.py
@@ -1,30 +1,30 @@
 # -*- coding: utf-8 -*-
 {
-    'name': '导入Excel',
-    'summary': """
+    "name": "导入Excel",
+    "summary": """
           导入Excel
        """,
     "website": "http://www.hxmelon.com",
     "license": "LGPL-3",
-    'version': '15.0.1',
+    "version": "15.0.1",
+    "category": "tools/数据导入",
+    "description": "数据导入",
+    "author": "melon",
+    "depends": ["base"],
     "images": ["static/description/image.png"],
-    'category': 'tools/数据导入',
-    'description': '数据导入',
-    'author': 'melon',
-    'depends': ['base'],
-    'data': [
-        'security/ir.model.access.csv',
-        'views/model_views.xml',
-        'wizard/import_data_wizard_view.xml',
-        'views/actions.xml',
-        'views/menu.xml',
+    "data": [
+        "security/ir.model.access.csv",
+        "views/model_views.xml",
+        "wizard/import_data_wizard_view.xml",
+        "views/actions.xml",
+        "views/menu.xml",
     ],
-    'assets': {
-        'web.assets_backend': [
+    "assets": {
+        "web.assets_backend": [
         ],
-        'web.assets_qweb': [
+        "web.assets_qweb": [
         ],
     },
-    'images': [
+    "images": [
     ],
 }

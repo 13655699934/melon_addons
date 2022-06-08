099b6aa3c76ad44af73f7eacdae3ad9e2a92df58
melon <13655699934@163.com>
2022-06-08 23:24:37 +0800

    odoo-demo8

--- export_excel_zip/__manifest__.py
+++ export_excel_zip/__manifest__.py
@@ -7,6 +7,7 @@
     'author': 'melon',
     'website': 'http://hxmelon.com/',
     'depends': ['base', 'mail'],
+    'license': 'LGPL-3',
     'version': '0.1',
     "images": ["static/description/img.png"],
     'data': [
@@ -15,7 +16,7 @@
         'views/demo_views.xml',
         'views/action_views.xml',
         'views/menu_views.xml',
-     ],
+    ],
     'installable': True,
     'application': True,
     'auto_install': False,

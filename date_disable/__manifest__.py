e10206e721fb5b9e14a83bf9003bf5c39952d08f
melon <13655699934@163.com>
2022-08-19 11:01:53 +0800

    odoo15-disable date

---
+++ date_disable/__manifest__.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Date Disable',
+    'summary': '日期字段禁用过去日期',
+    'category': '通用/tools',
+    'description': """日期字段禁用过去日期""",
+    'sequence': 666,
+    'author': 'melon',
+    'website': 'http://hxmelon.com/',
+    'depends': ['web'],
+    "images": ["static/description/img.png"],
+    'version': '15.0.1',
+    'data': [
+    ],
+    'demo': [
+    ],
+    'assets': {
+        'web.assets_backend': [
+            'date_disable/static/src/js/disable_date_widget.js',
+            'date_disable/static/src/js/disable_datetime_widget.js',
+        ],
+        'web._assets_primary_variables': [
+        ],
+        'web._assets_bootstrap': [
+        ],
+        'web.assets_qweb': [
+        ],
+        'web.report_assets_common': [
+        ],
+        'web._assets_common_styles': [
+        ],
+    },
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+    'license': 'LGPL-3',
+}

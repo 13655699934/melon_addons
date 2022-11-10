d4f7d0cb665bd5f0e212fb299989956f679703d3
melon <13655699934@163.com>
2022-11-10 14:16:51 +0800

    水印功能

--- attach_zip_download/__manifest__.py
+++ attach_zip_download/__manifest__.py
@@ -12,6 +12,8 @@
     'depends': ['base', 'mail'],
     'version': '15.0.1',
     "license": "LGPL-3",
+    'currency': "USD",
+    'price': 1.88,
     "images": ["static/description/img.png"],
     'data': [
         'security/ir.model.access.csv',
